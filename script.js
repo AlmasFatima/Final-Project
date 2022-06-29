dba743ff8327433e327b799125004cd4a93b1c33
Almas Fatima <103952533+AlmasFatima@users.noreply.github.com>
2022-06-29 16:15:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+    // var gallery = $('.gallery a').simpleLightbox();
+        
+     
+    // $(".single-item").slick({
+    //  autoplay:true,
+    //  autoplaySpeed: 2000,
+    //  arrows:true
+    // });
+
+    // $("#accordion").accordion();
+    //        collapsible:true;
+
+
+    //         $('.form').validate({
+        
+    //             rules: {
+    //                 name:{
+    //                     required: true,
+    //                     minlength: 3
+    //                 },
+    //                 email: {
+    //                     required: true,
+    //                     email:true,
+    //                 },
+    //                 subject: {
+    //                     minlength: 30,
+    //                     required: true
+    //                 },
+    //                 message: {
+    //                     maxlength: 100,
+    //                     minlength: 50,
+                        
+    //                 },
+    //             },
+    //             messages: {
+    //                 name:{
+    //                     minlength: "Name should be atleast 3 characters"
+    //                 },
+    //                 email:{
+    //                     email: "The email should be in the format: abc@domain.tld"
+    //                 },
+    //                 message:{
+    //                     minlength: "Message box should contain atleast 50 words.",
+    //                     maxlength:"Message box should not exceed 100 words"
+
+    //                 }
+    //             }
+    //         });
+    //         highlight=function(element){
+    //             $(element).addClass("c2")
+    //         }
+    //         unhighlight=function(elenment){
+    //             $(element).addClass("c3")
+    //         }
+     
+    //         $("#counter1").countMe(20,40);
+    //         $("#counter2").countMe(20,40);
+    //         $("#counter3").countMe(20,40);
+        
+        
+    //         $('.list').click(function(){
+    //             const value = $(this).attr('data-filter');
+    //             console.log(value) //all
+    //             if(value == 'all'){
+    //                 $('.itemBox').show('1000');
+    //             }
+    //             else{
+    //                     $('.itemBox').filter('.'+value).show('1000');
+    //                     $('.itemBox').not('.'+value).hide('1000');
+    //                 }
+                    
+    //             })
+    //              $('.list').click(function(){
+    //                      $(this).addClass('active').siblings().removeClass('active');
+    //                  })
+                    
